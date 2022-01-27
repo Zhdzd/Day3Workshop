ea2f3b10e10e91009451414821cfbc8a037d81bc
Zhdzd <zahidabdullah18@gmail.com>
2022-01-27 14:05:53 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {Todo } from './Todo';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'day_3_workshop_todo';
+  form: FormGroup;
+  tomorrow = new Date();
+  todosValues : Todo[] =[];
+  priorities =["low", "Medium","High"];
+
+  taskFormControl = new FormControl('', [Validators.required]);
+  priorityFormControl = new FormControl('', [Validators.required]);
+  dueDateFormControl = new FormControl('', [Validators.required]);
+
+  constructor(private fb: FormBuilder){
+    this.tomorrow.setDate(this.tomorrow.getDate()+1);
+    this.form = this.fb.group({
+      task: this.taskFormControl,
+      priority: this.priorityFormControl,
+      dueDate: this.dueDateFormControl
+    })
+  }
+  addTodo(){
+    console.log("Add todo");
+    let taskId = uuidv4();
+    let singleTodo = new Todo(
+      this.form.value.task,
+      this.form.value.priority,
+      this.form.value.dueDate,
+      taskId
+    )
+    this.todosValues.push(singleTodo);
+    this.taskFormControl.reset();
+    this.priorityFormControl.reset();
+    this.dueDateFormControl.reset();
+    localStorage.setItem(taskId, JSON.stringify(singleTodo))
+  }
 }
